8eee4afb1be180403a6f5c0cc429621ed8324a00
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-06-11 16:57:15 +0900

    feat: loginFilter,WebConfig 추가

---
+++ src/main/java/com/example/springboardproject/Config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.springboardproject.Config;
+
+import com.example.springboardproject.Filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        // Filter 등록
+        filterRegistrationBean.setFilter(new LoginFilter());
+        /**
+         * 필터의 우선순위 적용
+         * 0순위 - 인코딩 (요청과 응답에 올바른 문자셋 적용)
+         * 1순위 - 보안 (악성코드검사)
+         * 2순위 - 로그인
+         */
+        filterRegistrationBean.setOrder(2);
+        /**
+         *  전체 URL에 Filter 적용
+         *  "/api/*" -> /api 이후 모든 url에 필터적용
+         */
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
+}
---
+++ src/main/java/com/example/springboardproject/Filter/LoginFilter.java
@@ -0,0 +1,60 @@
+package com.example.springboardproject.Filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+    // 인증을 하지 않아도될 URL Path 배열
+    private static final String[] WHITE_LIST = {"/", "/members/signup", "/login", "/logout"};
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException
+    {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpRequest.getRequestURI();
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        log.info("로그인필터 로직 실행");
+        log.info("httpRequest {}", httpRequest);
+        log.info("httpResponse {}", httpResponse);
+
+        // isWhiteList 에 포함된 경우 ture -> !ture -> false
+        if (isWhiteList(requestURI)) {
+            // 로그인 확인 -> 로그인하면 session에 값이 저장되어 있다는 가정.
+            // 세션이 존재하면 가져온다. 세션이 없으면 session = null
+            HttpSession session = httpRequest.getSession(false);
+            // 로그인하지 않은 사용자인 경우
+            if (session == null || session.getAttribute("세션키값") == null) {
+                throw new RuntimeException();
+            }
+
+            log.info("로그인 성공");
+        }
+
+        // 1번경우 : whiteListURL에 등록된 URL 요청이면 바로 filterChain.doFilter()
+        // 2번경우 : 필터 로직 통과 후 다음 필터 호출 filterChain.doFilter()
+        // 다음 필터 없으면 Servlet -> Controller 호출하거나, 다른 Filter 존재시에 다음 Filter 호출
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    // 로그인 여부를 확인하는 URL인지 체크하는 메서드
+    private boolean isWhiteList(String requestURI) {
+
+        // request URI가 whiteListURL에 포함되는지 확인
+        // 포함되면 true 반환
+        // 포함되지 않으면 false 반환
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+
+
+}
